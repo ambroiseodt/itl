eae51801841713c056f26bcc16c90ad12f1ef1c6
Vivien Cabannes <vivien.cabannes@gmail.com>
2025-03-01 23:05:18 +0000

    agents placeholder

---
+++ src/nanollama/agents/__init__.py
@@ -0,0 +1,9 @@
+"""
+Module registering agents that the LLM can interact with.
+
+#### License
+This source code is licensed under the terms specified in the `LICENSE` file,
+located in the root directory of this repository.
+
+@ 2025, Meta
+"""
