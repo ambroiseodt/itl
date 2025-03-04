07811ed42b71bbb8ead5d624e17421a01f40a259
Vivien Cabannes <vivien.cabannes@gmail.com>
2025-03-04 00:07:13 +0000

    fix bug mandatory args & inheritance in dataclass

--- src/nanollama/model/blocklm.py
+++ src/nanollama/model/blocklm.py
@@ -47,16 +47,21 @@ class BlockLanguageModelConfig:
     # Block configuration
     implementation: str
     block: Any
-    nb_layers: int
 
     # Embedding parameters
-    emb_dim: int
+    emb_dim: int = 0
     vocab_size: int = 0
 
     # Model parameters
+    nb_layers: int = 0
     weight_tying: bool = False
     norm_eps: float = 1e-5
     init_std: float = None
+
+    def __post_init__(self):
+        assert self.block, "block should be specified"
+        assert self.emb_dim, "embedding dimension should be specified"
+        assert self.nb_layers, "number of layers should be specified"
 
     def __check_init__(self):
         """Check validity of arguments that may have been inherited."""
