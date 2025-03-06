d5f40caa91cee94033a9ee12e9c5132770b5aee0
ambroiseodt <ambroiseodonnattechnologie@gmail.com>
2025-03-06 18:21:09 +0100

    debug: remove double definition of FLEX_ATTENTION

--- src/nanollama/model/transformer.py
+++ src/nanollama/model/transformer.py
@@ -25,8 +25,6 @@ from .feedforward import FeedForward
 from .norm import RMSNorm
 
 logger = getLogger("nanollama")
-FLEX_ATTENTION = True
-
 
 # ------------------------------------------------------------------------------
 # Attention Cache
