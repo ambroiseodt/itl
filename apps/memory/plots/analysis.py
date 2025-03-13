8ebc1e1bf86fe20c6b976225fe71519993728408
Vivien Cabannes <vivien.cabannes@gmail.com>
2025-03-13 17:01:43 +0000

    adding plots

--- apps/memory/plots/analysis.py
+++ apps/memory/plots/analysis.py
@@ -2,6 +2,7 @@
 from logging import getLogger
 from pathlib import PosixPath
 
+import matplotlib.pyplot as plt
 import pandas as pd
 
 from nanollama.visualization.loader import get_config_info, get_task_ids, load_jsonl_to_numpy
@@ -44,3 +45,25 @@ df = pd.DataFrame(all_res)
 
 # %%
 
+yaxis = "loss_1000"
+keys = ["model.emb_dim", "model.nb_layers", "model.block.nb_heads"]
+
+# ind = (df['model.nb_layers'] == 4) & (df['model.emb_dim'] == 32) & (df['model.block.nb_heads'] == 4)
+ind = df["data.key"] == "qa"
+filtered_df = df[ind][["data.n_data", "data.key", yaxis] + keys]
+
+grouped = filtered_df.groupby(keys)
+# Plotting
+plt.figure(figsize=(10, 6))
+for name, group in grouped:
+    plt.plot(group["data.n_data"], group[yaxis], marker="o", linestyle="-", label=f"{name}")
+plt.title("Accuracy_1000 vs Data.n_data")
+plt.xlabel("Data.n_data")
+plt.ylabel("Accuracy_1000")
+plt.xscale("log")
+plt.legend(title="(data.key, emb_dim, nb_layers, nb_heads)", bbox_to_anchor=(1.05, 1), loc="upper left")
+plt.grid(True)
+plt.tight_layout()
+plt.show()
+
+# %%
