0d5c06b9eba33cdb794b32911e84a2ee1c564614
Vivien Cabannes <vivien.cabannes@gmail.com>
2025-03-18 10:16:05 +0000

    uploading some results

--- apps/memory/plots/analysis.py
+++ apps/memory/plots/analysis.py
@@ -1,8 +1,9 @@
 # %%
 from logging import getLogger
-from pathlib import PosixPath
+from pathlib import Path
 
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
 from nanollama.visualization.loader import get_config_info, get_task_ids, load_jsonl_to_numpy
@@ -10,11 +11,14 @@ from nanollama.visualization.loader import get_config_info, get_task_ids, load_j
 logger = getLogger("nanollama")
 
 
-# %%
+# %% Assuming you have run a grid, you can use the following code to concatenate the results
 
-steps = [20, 100, 1000, 10000]
+# put the path to the logging directory of your runs
+log_dir = Path.home() / "memory_grid"
 
-log_dir = PosixPath.home() / "grid_new"
+# steps = [20, 100, 1000, 10000]
+steps = ["best"]
+
 task_id = 1
 config_keys = ["data.n_data", "data.key", "model.emb_dim", "model.nb_layers", "model.block.nb_heads"]
 
@@ -29,9 +33,13 @@ for task_id in task_ids:
     step = data["step"]
     for snapshot in steps:
         try:
-            idx = (step == snapshot).argmax()
-            res[f"loss_{snapshot}"] = data["loss"][idx]
-            res[f"accuracy_{snapshot}"] = data["accuracy"][idx + 1]
+            if snapshot == "best":
+                res[f"loss_{snapshot}"] = np.nanmin(data["loss"].astype(float))
+                res[f"accuracy_{snapshot}"] = np.nanmax(data["accuracy"].astype(float))
+            else:
+                idx = (step == snapshot).argmax()
+                res[f"loss_{snapshot}"] = data["loss"][idx]
+                res[f"accuracy_{snapshot}"] = data["accuracy"][idx + 1]
         except Exception as e:
             logger.error(f"Error extracting loss for snapshot {snapshot}: {str(e)}")
 
@@ -43,13 +51,21 @@ for task_id in task_ids:
 
 df = pd.DataFrame(all_res)
 
-# %%
+# %% Otherwise you can load some results that I have saved in the repo
 
-yaxis = "loss_1000"
+path = Path(__file__).parents[3] / 'results' / 'grid1.csv'
+# df.to_csv(path)
+df = pd.read_csv(path)
+
+# %% From the dataframe, you can plot various things, I go for some basic plots here
+
+yaxis = "accuracy_best"
 keys = ["model.emb_dim", "model.nb_layers", "model.block.nb_heads"]
 
 # ind = (df['model.nb_layers'] == 4) & (df['model.emb_dim'] == 32) & (df['model.block.nb_heads'] == 4)
-ind = df["data.key"] == "qa"
+# key = "qa"
+key = "qatool"
+ind = df["data.key"] == key
 filtered_df = df[ind][["data.n_data", "data.key", yaxis] + keys]
 
 grouped = filtered_df.groupby(keys)
@@ -57,13 +73,11 @@ grouped = filtered_df.groupby(keys)
 plt.figure(figsize=(10, 6))
 for name, group in grouped:
     plt.plot(group["data.n_data"], group[yaxis], marker="o", linestyle="-", label=f"{name}")
-plt.title("Accuracy_1000 vs Data.n_data")
+plt.title(f"Accuracy_1000 vs Data.n_data {key}")
 plt.xlabel("Data.n_data")
 plt.ylabel("Accuracy_1000")
 plt.xscale("log")
-plt.legend(title="(data.key, emb_dim, nb_layers, nb_heads)", bbox_to_anchor=(1.05, 1), loc="upper left")
+# plt.legend(title="(data.key, emb_dim, nb_layers, nb_heads)", bbox_to_anchor=(1.05, 1), loc="upper left")
 plt.grid(True)
 plt.tight_layout()
 plt.show()
-
-# %%
--- apps/memory/plots/training_run.py
+++ apps/memory/plots/training_run.py
@@ -12,20 +12,20 @@ from nanollama.visualization.loader import load_results
 
 # %% Load results
 
-# log_dir = Path.home() / "memory"
-log_dir = Path.home() / "grid_first_runs"
+log_dir = Path.home() / "memory_grid"
 task_id = "1"
 metric_dir = log_dir / "metrics" / task_id
 
 dfs = {}
 dfs["train"] = load_results(metric_dir, "raw")
 dfs["profiler"] = load_results(metric_dir, "prof")
-dfs["eval"] = load_results(metric_dir, "evas")
+# dfs["eval"] = load_results(metric_dir, "evas")
 
 # %% Plot some of them with Plotly
 
 # should the metric to plot
-metrics = ["train.loss", "train.ts", "eval.loss"]
+# metrics = ["train.loss", "train.ts", "eval.loss"]
+metrics = ["train.loss", "train.ts", "train.accuracy"]
 
 # create a figure with subplots (you can add arguments such as vspace, ...)
 n_cols = 2
@@ -36,7 +36,7 @@ fig = make_subplots(rows=n_rows, cols=n_cols, subplot_titles=metrics)
 # Add traces for each column
 for i, metric in enumerate(metrics):
     src, metric = metric.split(".")
-    df = dfs[src][["step", metric]]
+    df = dfs[src][["step", metric]].dropna()
     row = i // n_cols + 1
     col = i % n_cols + 1
     ax = fig.add_trace(go.Scatter(x=df["step"], y=df[metric], mode="lines", name=metric), row=row, col=col)
