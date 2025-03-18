d9e4d5bc4cf0629734f32576990984cca5828127
Vivien Cabannes <vivien.cabannes@gmail.com>
2025-03-18 14:26:48 +0000

    nicer plots

--- apps/memory/plots/analysis.py
+++ apps/memory/plots/analysis.py
@@ -11,7 +11,9 @@ from nanollama.visualization.loader import get_config_info, get_task_ids, load_j
 logger = getLogger("nanollama")
 
 
+# ------------------------------------------------------------------------------
 # %% Assuming you have run a grid, you can use the following code to concatenate the results
+# ------------------------------------------------------------------------------
 
 # put the path to the logging directory of your runs
 log_dir = Path.home() / "memory_grid"
@@ -20,7 +22,7 @@ log_dir = Path.home() / "memory_grid"
 steps = ["best"]
 
 task_id = 1
-config_keys = ["data.n_data", "data.key", "model.emb_dim", "model.nb_layers", "model.block.nb_heads"]
+config_keys = ["data.nb_data", "data.key", "model.emb_dim", "model.nb_layers", "model.block.nb_heads"]
 
 task_ids = get_task_ids(log_dir)
 all_res = []
@@ -44,40 +46,171 @@ for task_id in task_ids:
             logger.error(f"Error extracting loss for snapshot {snapshot}: {str(e)}")
 
     # add meta data
-    res |= get_config_info(log_dir, task_id, keys=config_keys)
+    res |= get_config_info(log_dir, task_id, keys=config_keys, num_params=True)
     all_res.append(res)
 
     print(f"Task {task_id} done")
 
 df = pd.DataFrame(all_res)
 
-# %% Otherwise you can load some results that I have saved in the repo
 
-path = Path(__file__).parents[3] / 'results' / 'grid1.csv'
+# ------------------------------------------------------------------------------
+# %% Otherwise you can load some results that I have saved in the repo
+# ------------------------------------------------------------------------------
+
+path = Path(__file__).parents[3] / "results" / "grid2.csv"
 # df.to_csv(path)
 df = pd.read_csv(path)
 
-# %% From the dataframe, you can plot various things, I go for some basic plots here
 
+# ------------------------------------------------------------------------------
+# %% From the dataframe, you can plot various things, I go for some basic plots here
+# ------------------------------------------------------------------------------
+
+xaxis = "data.nb_data"
 yaxis = "accuracy_best"
 keys = ["model.emb_dim", "model.nb_layers", "model.block.nb_heads"]
+for key in ["qa", "qatool"]:
+    ind = df["data.key"] == key
+    filtered_df = df[ind][["data.key", xaxis, yaxis] + keys]
 
-# ind = (df['model.nb_layers'] == 4) & (df['model.emb_dim'] == 32) & (df['model.block.nb_heads'] == 4)
-# key = "qa"
-key = "qatool"
-ind = df["data.key"] == key
-filtered_df = df[ind][["data.n_data", "data.key", yaxis] + keys]
+    grouped = filtered_df.groupby(keys)
+    # Plotting
+    plt.figure(figsize=(10, 6))
+    for name, group in grouped:
+        plt.plot(group[xaxis], group[yaxis], marker="o", linestyle="-", label=f"{name}")
+    plt.title(f"Performance for {key}")
+    plt.xlabel(xaxis)
+    plt.ylabel(yaxis)
+    plt.xscale("log")
+    # plt.legend(title="(data.key, emb_dim, nb_layers, nb_heads)", bbox_to_anchor=(1.05, 1), loc="upper left")
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
 
-grouped = filtered_df.groupby(keys)
-# Plotting
-plt.figure(figsize=(10, 6))
-for name, group in grouped:
-    plt.plot(group["data.n_data"], group[yaxis], marker="o", linestyle="-", label=f"{name}")
-plt.title(f"Accuracy_1000 vs Data.n_data {key}")
-plt.xlabel("Data.n_data")
-plt.ylabel("Accuracy_1000")
-plt.xscale("log")
-# plt.legend(title="(data.key, emb_dim, nb_layers, nb_heads)", bbox_to_anchor=(1.05, 1), loc="upper left")
-plt.grid(True)
+
+# ------------------------------------------------------------------------------
+# %% Add a measure of facts memorized
+# ------------------------------------------------------------------------------
+
+df["nb_facts"] = df["data.nb_data"] * df["accuracy_best"]
+
+
+# ------------------------------------------------------------------------------
+# %% Show the number of learned facts vs the number of parameters
+# ------------------------------------------------------------------------------
+
+fig, ax = plt.subplots(figsize=(9, 6))
+
+# with tool
+ind = (df["data.key"] == "qatool") & (df["data.nb_data"] == 10_000)
+tmp = df[ind]
+ax.scatter(tmp["nb_facts"], tmp["nb_params"], label="With tool", alpha=0.7)
+
+# without tool
+ind = (df["data.key"] == "qa") & (df["data.nb_data"] == 10_000)
+tmp = df[ind]
+ax.scatter(tmp["nb_facts"], tmp["nb_params"], label="Without tools", alpha=0.7)
+
+# metadata
+ax.set_title("Number of learned facts (on 10,000)")
+ax.set_xlabel("Number of facts")
+ax.set_ylabel("Number of parameters")
+ax.set_xscale("log")
+ax.set_yscale("log")
+ax.legend()
+
+# Show plot
 plt.tight_layout()
 plt.show()
+
+
+# ------------------------------------------------------------------------------
+# %% Similar plot that is crisper
+# ------------------------------------------------------------------------------
+
+fig, axes = plt.subplots(1, 2, figsize=(8, 4))
+x = df['data.nb_data'].unique()
+
+# with tool
+ind = (df["data.key"] == "qatool") & (df["accuracy_best"] >= 0.99)
+y_min, y_mean = [], []
+for nb_data in x:
+    tmp = df[(df["data.nb_data"] == nb_data) & ind]["nb_params"]
+    y_min.append(tmp.min())
+    y_mean.append(tmp.mean())
+axes[0].plot(x, y_min, label="With tool", linestyle="--", marker="o")
+axes[1].plot(x, y_mean, label="With tool", linestyle="--", marker="o")
+
+# without tool
+ind = (df["data.key"] == "qa") & (df["accuracy_best"] >= 0.99)
+y_min, y_mean = [], []
+for nb_data in x:
+    tmp = df[(df["data.nb_data"] == nb_data) & ind]["nb_params"]
+    y_min.append(tmp.min())
+    y_mean.append(tmp.mean())
+axes[0].plot(x, y_min, label="Without tool", linestyle="--", marker="o")
+axes[1].plot(x, y_mean, label="Without tool", linestyle="--", marker="o")
+
+# metadata
+axes[0].set_ylabel("Min nb params s.t. accuracy > 0.99")
+axes[1].set_ylabel("Mean nb params s.t. accuracy > 0.99")
+for ax in axes:
+    ax.set_xlabel("Number of data")
+    ax.set_xscale("log")
+    ax.set_yscale("log")
+    ax.grid()
+    ax.legend()
+
+# Show plot
+plt.tight_layout()
+plt.show()
+
+
+# ------------------------------------------------------------------------------
+# %% Look at the influence of the number of heads
+# ------------------------------------------------------------------------------
+
+fig, axes = plt.subplots(1, 2, figsize=(8, 4))
+x = df['data.nb_data'].unique()
+
+# key = "model.block.nb_heads"
+# key = "model.nb_layers"
+key = "model.emb_dim"
+val = df[key].unique()
+
+# with tool
+for i in val:
+    params_ind = df[key] == i
+    ind = (df["data.key"] == "qatool") & (df["accuracy_best"] >= 0.99)
+    ind &= params_ind
+    y_min, y_mean = [], []
+    for nb_data in x:
+        tmp = df[(df["data.nb_data"] == nb_data) & ind]["nb_params"]
+        y_min.append(tmp.min())
+    axes[0].plot(x, y_min, label=f"With tool, {i} {key}", linestyle="--", marker="o")
+
+    # without tool
+    ind = (df["data.key"] == "qa") & (df["accuracy_best"] >= 0.99)
+    ind &= params_ind
+    y_min, y_mean = [], []
+    for nb_data in x:
+        tmp = df[(df["data.nb_data"] == nb_data) & ind]["nb_params"]
+        y_min.append(tmp.min())
+    axes[1].plot(x, y_min, label=f"Without tool, {i} {key}", linestyle="--", marker="o")
+
+# metadata
+axes[0].set_ylabel("Min nb params s.t. accuracy > 0.99")
+axes[1].set_ylabel("Min nb params s.t. accuracy > 0.99")
+for ax in axes:
+    ax.set_xlabel("Number of data")
+    ax.set_xscale("log")
+    ax.set_yscale("log")
+    ax.grid()
+    ax.legend()
+
+# Show plot
+plt.tight_layout()
+plt.show()
+
+# %%
--- apps/memory/plots/training_run.py
+++ apps/memory/plots/training_run.py
@@ -5,15 +5,18 @@ Generate Plots to visualize training runs
 # %% Imports
 from pathlib import Path
 
+import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
 from nanollama.visualization.loader import load_results
 
+# ------------------------------------------------------------------------------
 # %% Load results
+# ------------------------------------------------------------------------------
 
 log_dir = Path.home() / "memory_grid"
-task_id = "1"
+task_id = "1162"
 metric_dir = log_dir / "metrics" / task_id
 
 dfs = {}
@@ -21,11 +24,13 @@ dfs["train"] = load_results(metric_dir, "raw")
 dfs["profiler"] = load_results(metric_dir, "prof")
 # dfs["eval"] = load_results(metric_dir, "evas")
 
+
+# ------------------------------------------------------------------------------
 # %% Plot some of them with Plotly
+# ------------------------------------------------------------------------------
 
 # should the metric to plot
-# metrics = ["train.loss", "train.ts", "eval.loss"]
-metrics = ["train.loss", "train.ts", "train.accuracy"]
+metrics = ["train.loss", "train.accuracy", "train.ts"]
 
 # create a figure with subplots (you can add arguments such as vspace, ...)
 n_cols = 2
@@ -47,5 +52,29 @@ fig.update_layout(showlegend=False, height=300 * n_rows, width=600 * n_cols)
 fig.show()
 
 
-#
+# ------------------------------------------------------------------------------
+# %% Plotting with matplotlib instead of plotly
+# ------------------------------------------------------------------------------
+
+fig, axes = plt.subplots(n_rows, n_cols, figsize=(6 * n_cols, 3 * n_rows))
+axes = axes.flatten()
+
+# Plot each metric
+for i, metric in enumerate(metrics):
+    src, metric = metric.split(".")
+    df = dfs[src][["step", metric]].dropna()
+    ax = axes[i]
+    ax.plot(df["step"], df[metric])
+    ax.set_xlabel("step")
+    ax.set_ylabel(metric)
+    ax.set_yscale("log")
+    ax.set_xscale("log")
+
+# Remove empty subplots
+for i in range(len(metrics), n_rows * n_cols):
+    fig.delaxes(axes[i])
+
+plt.tight_layout()
+plt.show()
+
 # %%
