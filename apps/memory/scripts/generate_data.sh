6fe4e8fe4f97c9014fd9c31f152ce5d4a872c16c
ambroiseodt <ambroiseodonnattechnologie@gmail.com>
2025-05-28 18:22:12 +0200

    cosmetic

--- apps/memory/scripts/generate_data.sh
+++ apps/memory/scripts/generate_data.sh
@@ -1,10 +1,10 @@
 #!usr/bin/bash
 
-# This file is useful to generate data for the memory app. 
-# Formatting the database as a SQLLite database is also possible. 
+# This file is useful to generate data for the memory app.
+# Formatting the database as a SQLLite database is also possible.
 # To that end, run the following command in the terminal from the root directory of the project.
 # ```shell
-# cd <path_to_rool_folder>
+# cd <path_to_root_folder>
 # bash apps/memory/bash/generate_data.sh
 # ```
 
