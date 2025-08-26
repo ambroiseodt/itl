5339f5e6089f8c19e564acdabd1324b4686398fb
ambroiseodt <ambroiseodonnattechnologie@gmail.com>
2025-08-26 15:13:02 +0000

    remove useless file

--- apps/large_scale/training/__init__.py
+++
@@ -1,6 +0,0 @@
-# This source code is licensed under the terms specified in the `LICENSE` file.
-"""
-Module for supervised fine-tuning (SFT) with in-weight and in-tool settings.
-
-@ 2025, Meta
-"""
