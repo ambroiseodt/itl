ca7cd81968b1d7c120f20c0df7fa442bd87ebaf5
ambroiseodt <ambroiseodonnattechnologie@gmail.com>
2025-08-26 08:21:49 +0000

    cosmetic

--- src/nanollama/__init__.py
+++ src/nanollama/__init__.py
@@ -1,6 +1,6 @@
 # This source code is licensed under the terms specified in the `LICENSE` file.
 """
-NanoLLaMa: a module to train state-of-the-art small language models with a focus on hackability and simplicity.
+NanoLlama: a module to train state-of-the-art small language models with a focus on hackability and simplicity.
 
 @ 2025, Meta
 """
