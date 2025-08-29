f3e0fe402bc0804391399069e285c7044c646319
ambroiseodt <ambroiseodonnattechnologie@gmail.com>
2025-08-29 12:29:27 +0000

    update paths

--- apps/large_scale/scripts/smollm_experiments.sh
+++ apps/large_scale/scripts/smollm_experiments.sh
@@ -6,51 +6,49 @@
 #
 
 #=======================================================
-#----------     SMOL IN-WEIGHT TRAINING     ------------
+#----------    SmolLM in-weight training    ------------
 #=======================================================
 
 # --- Training: Model: Smol 135M, Dataset_sizes: [500, 1000, 5000, 10000, 50000]
-accelerate launch -m apps.large_scale.training.finetune_parallelized --save_dir "smollm" --run_name "sft_Smol135M_facts=8-epochs=2-batch=8-gradAcc=2-LR=1e-3-loraR=0-loraA=0-weight"
+accelerate launch -m apps.large_scale.training.finetune_parallelized --save_dir "smollm" --run_name "sft_Smol135M_facts=500-epochs=30-batch=64-gradAcc=2-LR=1e-3-loraR=0-loraA=0-weight"
+accelerate launch -m apps.large_scale.training.finetune_parallelized --save_dir "smollm" --run_name "sft_Smol135M_facts=1000-epochs=30-batch=64-gradAcc=2-LR=1e-3-loraR=0-loraA=0-weight"
+accelerate launch -m apps.large_scale.training.finetune_parallelized --save_dir "smollm" --run_name "sft_Smol135M_facts=5000-epochs=30-batch=64-gradAcc=2-LR=1e-3-loraR=0-loraA=0-weight"
+accelerate launch -m apps.large_scale.training.finetune_parallelized --save_dir "smollm" --run_name "sft_Smol135M_facts=10000-epochs=30-batch=64-gradAcc=2-LR=1e-3-loraR=0-loraA=0-weight"
+accelerate launch -m apps.large_scale.training.finetune_parallelized --save_dir "smollm" --run_name "sft_Smol135M_facts=50000-epochs=30-batch=64-gradAcc=2-LR=1e-3-loraR=0-loraA=0-weight"
 
-# accelerate launch apps/large_scale/training/finetune_parallelized.py --run_name "sft_Smol135M_facts=500-epochs=30-batch=64-gradAcc=2-LR=1e-3-loraR=0-loraA=0-weight"
-# accelerate launch apps/large_scale/training/finetune_parallelized.py --run_name "sft_Smol135M_facts=1000-epochs=30-batch=64-gradAcc=2-LR=1e-3-loraR=0-loraA=0-weight"
-# accelerate launch apps/large_scale/training/finetune_parallelized.py --run_name "sft_Smol135M_facts=5000-epochs=30-batch=64-gradAcc=2-LR=1e-3-loraR=0-loraA=0-weight"
-# accelerate launch apps/large_scale/training/finetune_parallelized.py --run_name "sft_Smol135M_facts=10000-epochs=30-batch=64-gradAcc=2-LR=1e-3-loraR=0-loraA=0-weight"
-# accelerate launch apps/large_scale/training/finetune_parallelized.py --run_name "sft_Smol135M_facts=50000-epochs=30-batch=64-gradAcc=2-LR=1e-3-loraR=0-loraA=0-weight"
+# --- Training: Model: Smol 360M, Dataset_sizes: [500, 1000, 5000, 10000, 50000]
+accelerate launch -m apps.large_scale.training.finetune_parallelized --save_dir "smollm" --run_name "sft_Smol360M_facts=500-epochs=30-batch=64-gradAcc=2-LR=1e-3-loraR=0-loraA=0-weight"
+accelerate launch -m apps.large_scale.training.finetune_parallelized --save_dir "smollm" --run_name "sft_Smol360M_facts=1000-epochs=30-batch=64-gradAcc=2-LR=1e-3-loraR=0-loraA=0-weight"
+accelerate launch -m apps.large_scale.training.finetune_parallelized --save_dir "smollm" --run_name "sft_Smol360M_facts=5000-epochs=30-batch=64-gradAcc=2-LR=1e-3-loraR=0-loraA=0-weight"
+accelerate launch -m apps.large_scale.training.finetune_parallelized --save_dir "smollm" --run_name "sft_Smol360M_facts=10000-epochs=30-batch=64-gradAcc=2-LR=1e-3-loraR=0-loraA=0-weight"
+accelerate launch -m apps.large_scale.training.finetune_parallelized --save_dir "smollm" --run_name "sft_Smol360M_facts=50000-epochs=30-batch=64-gradAcc=2-LR=1e-3-loraR=0-loraA=0-weight"
 
-# # --- Training: Model: Smol 360M, Dataset_sizes: [500, 1000, 5000, 10000, 50000]
-# accelerate launch apps/large_scale/training/finetune_parallelized.py --run_name "sft_Smol360M_facts=500-epochs=30-batch=64-gradAcc=2-LR=1e-3-loraR=0-loraA=0-weight"
-# accelerate launch apps/large_scale/training/finetune_parallelized.py --run_name "sft_Smol360M_facts=1000-epochs=30-batch=64-gradAcc=2-LR=1e-3-loraR=0-loraA=0-weight"
-# accelerate launch apps/large_scale/training/finetune_parallelized.py --run_name "sft_Smol360M_facts=5000-epochs=30-batch=64-gradAcc=2-LR=1e-3-loraR=0-loraA=0-weight"
-# accelerate launch apps/large_scale/training/finetune_parallelized.py --run_name "sft_Smol360M_facts=10000-epochs=30-batch=64-gradAcc=2-LR=1e-3-loraR=0-loraA=0-weight"
-# accelerate launch apps/large_scale/training/finetune_parallelized.py --run_name "sft_Smol360M_facts=50000-epochs=30-batch=64-gradAcc=2-LR=1e-3-loraR=0-loraA=0-weight"
-
-# # --- Training: Model: Smol 1.7B, Dataset_sizes: [500, 1000, 5000, 10000, 50000]
-# accelerate launch apps/large_scale/training/finetune_parallelized.py --run_name "sft_Smol1.7B_facts=500-epochs=30-batch=64-gradAcc=2-LR=3e-4-loraR=0-loraA=0-weight"
-# accelerate launch apps/large_scale/training/finetune_parallelized.py --run_name "sft_Smol1.7B_facts=1000-epochs=30-batch=64-gradAcc=2-LR=3e-4-loraR=0-loraA=0-weight"
-# accelerate launch apps/large_scale/training/finetune_parallelized.py --run_name "sft_Smol1.7B_facts=5000-epochs=30-batch=64-gradAcc=2-LR=3e-4-loraR=0-loraA=0-weight"
-# accelerate launch apps/large_scale/training/finetune_parallelized.py --run_name "sft_Smol1.7B_facts=10000-epochs=30-batch=64-gradAcc=2-LR=3e-4-loraR=0-loraA=0-weight"
-# accelerate launch apps/large_scale/training/finetune_parallelized.py --run_name "sft_Smol1.7B_facts=50000-epochs=30-batch=64-gradAcc=2-LR=3e-4-loraR=0-loraA=0-weight"
+# --- Training: Model: Smol 1.7B, Dataset_sizes: [500, 1000, 5000, 10000, 50000]
+accelerate launch -m apps.large_scale.training.finetune_parallelized --save_dir "smollm" --run_name "sft_Smol1.7B_facts=500-epochs=30-batch=64-gradAcc=2-LR=3e-4-loraR=0-loraA=0-weight"
+accelerate launch -m apps.large_scale.training.finetune_parallelized --save_dir "smollm" --run_name "sft_Smol1.7B_facts=1000-epochs=30-batch=64-gradAcc=2-LR=3e-4-loraR=0-loraA=0-weight"
+accelerate launch -m apps.large_scale.training.finetune_parallelized --save_dir "smollm" --run_name "sft_Smol1.7B_facts=5000-epochs=30-batch=64-gradAcc=2-LR=3e-4-loraR=0-loraA=0-weight"
+accelerate launch -m apps.large_scale.training.finetune_parallelized --save_dir "smollm" --run_name "sft_Smol1.7B_facts=10000-epochs=30-batch=64-gradAcc=2-LR=3e-4-loraR=0-loraA=0-weight"
+accelerate launch -m apps.large_scale.training.finetune_parallelized --save_dir "smollm" --run_name "sft_Smol1.7B_facts=50000-epochs=30-batch=64-gradAcc=2-LR=3e-4-loraR=0-loraA=0-weight"
 
 
-# #=======================================================
-# #----------     SMOL IN-TOOL TRAINING     --------------
-# #=======================================================
+#=======================================================
+#----------     SmolLM in-tool training     ------------
+#=======================================================
 
-# # --- Training: Model: Smol 135M, Dataset_sizes: [500]
-# accelerate launch apps/large_scale/training/finetune_parallelized.py --run_name "sft_Smol135M_facts=500-epochs=15-batch=64-gradAcc=1-LR=1e-4-loraR=0-loraA=0-tool"
+# --- Training: Model: Smol 135M, Dataset_sizes: [500]
+accelerate launch -m apps.large_scale.training.finetune_parallelized --save_dir "smollm" --run_name "sft_Smol135M_facts=500-epochs=15-batch=64-gradAcc=1-LR=1e-4-loraR=0-loraA=0-tool"
 
-# # --- Training: Model: Smol 360M, Dataset_sizes: [500]
-# accelerate launch apps/large_scale/training/finetune_parallelized.py --run_name "sft_Smol360M_facts=500-epochs=15-batch=64-gradAcc=2-LR=1e-3-loraR=0-loraA=0-tool"
+# --- Training: Model: Smol 360M, Dataset_sizes: [500]
+accelerate launch -m apps.large_scale.training.finetune_parallelized --save_dir "smollm" --run_name "sft_Smol360M_facts=500-epochs=15-batch=64-gradAcc=2-LR=1e-3-loraR=0-loraA=0-tool"
 
-# # --- Training: Model: Smol 1.7B, Dataset_sizes: [500]
-# accelerate launch apps/large_scale/training/finetune_parallelized.py --run_name "sft_Smol1.7B_facts=500-epochs=15-batch=64-gradAcc=1-LR=5e-5-loraR=0-loraA=0-tool"
+# --- Training: Model: Smol 1.7B, Dataset_sizes: [500]
+accelerate launch -m apps.large_scale.training.finetune_parallelized --save_dir "smollm" --run_name "sft_Smol1.7B_facts=500-epochs=15-batch=64-gradAcc=1-LR=5e-5-loraR=0-loraA=0-tool"
 
 
-# #=======================================================
-# #----------          EVALUATION           --------------
-# #=======================================================
+#=======================================================
+#----------          Evaluation           --------------
+#=======================================================
 
 python -m apps.large_scale.evaluation.eval_recall --model_dir "smollm" --eval_dir "smollm"
-python -m apps.large_scale.evaluation.eval_hellaswag --mode "checkpoints" --base_model_family "smollm" --model_dir "smollm" --eval_dir "smollm"
+python -m apps.large_scale.evaluation.eval_hellaswag --mode "both" --base_model_family "smollm" --model_dir "smollm" --eval_dir "smollm"
 python -m apps.large_scale.evaluation.eval_kl --model_dir "smollm" --eval_dir "smollm"
